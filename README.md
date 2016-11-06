b4b7a1f224a48ae0ee5508464ea43a72818bed6b
NIRUTHYA VENKATESAN <niruthyavenkatesan@NIRUTHYAs-Air.home>
2016-11-06 14:23:21 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# centos67_nginx
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'centos67_nginx'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures centos67_nginx'
+long_description 'Installs/Configures centos67_nginx'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/centos67_nginx/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/centos67_nginx' if respond_to?(:source_url)
